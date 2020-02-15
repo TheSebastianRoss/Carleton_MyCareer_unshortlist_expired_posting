1ba79bf3ebeacaccc5ba3f6e46b36a48944d1375
Sebastian Ross <16342633+TheSebastianRoss@users.noreply.github.com>
2020-02-15 15:39:22 -0500

    Remove unnecessary function wrapper

--- unshortlist.js
+++ unshortlist.js
@@ -1,8 +1,8 @@
-(_ => orbisApp.buildForm({
+orbisApp.buildForm({
   'action': '_-_-xiMRXZ2Qq2ageU4ROJ-ZOQpb1tTI_lOi30gCOY77dFyKoV5cGOWRJ6tFZBMkY6r-1IqmVf6AuoHbHuHu1aONdEsCcO2NwCrtU4ejvAyJsP82Wn8TUAj5GyeOutxtSPoOiW7Jz0Z5wiA5Y7rPoz-Iq3kOyo4uXIk7SXk4LwmQ9qWK-po6DyNCrQ',
   'postingId': document.getElementsByName("postingId")[0].value,
   'singleJobMode': 'true',
   'searchType': '',
   'accessToPostings': 'infoForPostings',
   'initialSearchAction': 'displayMyProgramJobs'
-}, '', '').submit())()
+}, '', '').submit()
